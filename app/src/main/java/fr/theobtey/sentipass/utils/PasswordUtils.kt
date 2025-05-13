22231ff22080a6e391bed15293e29eb91f3e47b0
unknown <95309102+TheObtey@users.noreply.github.com>
2025-05-13 11:02:18 +0200

    Modify password strength test algorithm

--- app/src/main/java/fr/theobtey/sentipass/utils/PasswordUtils.kt
+++ app/src/main/java/fr/theobtey/sentipass/utils/PasswordUtils.kt
@@ -9,13 +9,19 @@ fun getPasswordStrength(password: String): Pair<String, Int> {
     val length = password.length
     val hasUpper = password.any { it.isUpperCase() }
     val hasLower = password.any { it.isLowerCase() }
-    val digitCount = password.count { it.isDigit() }
-    val context = LocalContext.current
-    val paswordStrength = context.resources.getStringArray(R.array.password_strength_levels)
+    val hasDigit = password.any { it.isDigit() }
+    val hasSpecial = password.any { !it.isLetterOrDigit() }
 
-    return when {
-        length >= 14 && hasUpper && hasLower && digitCount >= 4 -> paswordStrength[2] to R.color.flashy_green
-        length in 8..13 && hasUpper && hasLower && digitCount >= 1 -> paswordStrength[1] to R.color.flashy_yellow
-        else -> paswordStrength[0] to R.color.flashy_red
+    val score = listOf(hasUpper, hasLower, hasDigit, hasSpecial).count { it } + when {
+        length >= 14 -> 2
+        length >= 10 -> 1
+        else -> 0
+    }
+
+    return when (score) {
+        in 0..2 -> "Weak password" to R.color.flashy_red
+        in 3..4 -> "Medium password" to R.color.flashy_yellow
+        else -> "Strong password" to R.color.flashy_green
     }
 }
+
